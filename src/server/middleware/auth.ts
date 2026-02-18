17827430a9b443f9231b264e0ca5246082410635
EnO33 <mattmar33@gmail.com>
2026-02-18 16:56:14 +0100

    feat: add middleware

---
+++ src/server/middleware/auth.ts
@@ -0,0 +1,12 @@
+import { createMiddleware } from '@tanstack/react-start'
+import { auth } from '@clerk/tanstack-react-start/server'
+
+export const authMiddleware = createMiddleware({ type: 'function' }).server(
+  async ({ next }) => {
+    const { userId } = await auth()
+    if (!userId) {
+      throw new Error('Unauthorized')
+    }
+    return next({ context: { userId } })
+  },
+)
